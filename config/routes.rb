04fadc942f1edcc8693fd61b77d8b60ad57b87d6
Peter Njuguna <Ptahbn@gmail.com>
2022-12-20 23:26:30 +0300

    Fix linters and Implementing the Login and Registration Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
+  # devise_for :users
+  # root to: "devise/sessions#new"
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_in' => 'devise/sessions#new'
+    get '/users/sign_up' => 'devise/registrations#new'
+
+    get '/users/password/new' => 'devise/passwords#new'
+    get '/users/password/edit' => 'devise/passwords#edit'
+  end
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
